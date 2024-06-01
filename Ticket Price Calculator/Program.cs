6e1674f376f672a7e924876bf6fde6cc5b0cc488
khissdhave <davidamediku28@gmail.com>
2024-06-01 23:48:13 +0000

    Commit for Ticket Price Calculator

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+decimal price, discountedPrice;
+int age;
+
+price = 10.00m;
+discountedPrice = 7.00m;
+
+Console.WriteLine("Enter your age: ");
+
+age = Convert.ToInt32(Console.ReadLine());
+
+while (age < 0)
+{
+    Console.WriteLine("The number you have entered is a negative number");
+    Console.WriteLine("Enter your right age: ");
+    age = Convert.ToInt32(Console.ReadLine());
+}
+
+if (age >= 65 || age <= 12)
+{
+    Console.WriteLine("Price of ticket is GH₵ " + discountedPrice);
+}else
+{
+    Console.WriteLine("Price of ticket is GH₵ " + price);
+}
